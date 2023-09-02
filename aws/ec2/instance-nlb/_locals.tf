b2fb3f075e4f92b0d555257400d019818b7881cf
benitodarder <benitodarder@gmail.com>
2023-09-02 21:18:07 +0200

    Removes unused module

--- aws/ec2/instance-nlb/_locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  workspace_path = "./workspaces/${terraform.workspace}/configuration.yaml"
-  workspace      = file(local.workspace_path)
-  settings       = yamldecode(local.workspace)
-}
--- aws/ec2/instance-nlb/workspaces/sample/backend.tfvars
+++
@@ -1,4 +0,0 @@
-region         = <Backend region>
-bucket         = <Backend bucket name>
-key            = <Module key>.tfstate
-dynamodb_table = <DynamoDB table>
