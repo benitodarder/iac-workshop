dbcaeea36e3978f6d476e61c5b07ac9da8aaceb7
benitodarder <benitodarder@gmail.com>
2023-10-11 20:40:14 +0200

    Comment sample code to prevent error

--- vsphere/tag/tag-report/_locals.tf
+++ vsphere/tag/tag-report/_locals.tf
@@ -4,14 +4,13 @@ locals {
   settings       = yamldecode(local.workspace)
 }
 
-data "vsphere_tag_category" "this" {
-  count = length(try(local.settings.tags_map, {}))
-  name  = keys(local.settings.tags_map)[count.index]
-}
+#data "vsphere_tag_category" "this" {
+#  count = length(try(local.settings.tags_map, {}))
+#  name  = keys(local.settings.tags_map)[count.index]
+#}
 
-data "vsphere_tag" "this" {
-  count       = length(try(local.settings.tags_map, {}))
-  name        = local.settings.tags_map[keys(local.settings.tags_map)[count.index]]
-  category_id = data.vsphere_tag_category.this[count.index].id
-
-}
+#data "vsphere_tag" "this" {
+#  count       = length(try(local.settings.tags_map, {}))
+#  name        = local.settings.tags_map[keys(local.settings.tags_map)[count.index]]
+#  category_id = data.vsphere_tag_category.this[count.index].id
+#}
